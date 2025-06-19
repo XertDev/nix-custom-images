2b88fc04d8480d7d2158bf76b17c493aef4c0ba7
Xert <xertdev@gmail.com>
2025-06-19 12:48:53 +0200

    Fix args when arg name is config

--- internal/typed-function.nix
+++ internal/typed-function.nix
@@ -5,6 +5,7 @@ let
   function = definition.function;
 
   evaluatedConfig = lib.evalModules {
-    modules = [ optionsModule args { config._module.check = true; } ];
+    modules =
+      [ optionsModule { config = args; } { config._module.check = true; } ];
   };
 in function { inherit (evaluatedConfig) config; }
